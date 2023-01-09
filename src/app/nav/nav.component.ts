cdd91a3a8902fed67d5986e1931857ae203066cd
FenrirWolf4566 <bdezordo@gmail.com>
2023-01-09 22:56:14 +0100

    Nav update

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenStorageService } from '../services/token-storage.service';
 import 'bootstrap';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  //imports: [NgbDropdownModule],
 })
 export class NavComponent {
 
